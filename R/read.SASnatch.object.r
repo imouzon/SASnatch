5bc2999b9f7d0e3c8a085d1d208fef63bc670231
imouzon <user@imouzon.student.iastate.edu>
2014-04-25 14:12:43 -0500

    no log files created by SAS code atm
    so removed from S4 object

--- R/read.SASnatch.object.r
+++ R/read.SASnatch.object.r
@@ -15,41 +15,41 @@ read.SASnatch.object<- function (chunk.name='',SAS2R.names='',SAS2R.type='csv'){
    files <- list.files(path=SAScache.directory)
    files <- files[grepl(chunk.name,files)]
 
-   ##get .html files
+   #get .html files
    html.files <- paste(SAScache.directory,files[grepl('.html',files)],sep='/')
    html.results <- lapply(1:length(html.files),function(i) paste(scan(file=html.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
-   ##get .tex files
+   #get .tex files
    tex.files <- paste(SAScache.directory,files[grepl('.tex',files)],sep='/')
    tex.results <- lapply(1:length(tex.files),function(i) paste(scan(file=tex.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
-   ##make new snatchResults S4 object
+   #make new snatchResults S4 object
    SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results)
 
-   return(SASnatch.results)
-
-   ##get .sas files
-   #code.files <- paste(SAScache.directory,files[grepl('.sas',files)],sep='/')
-   #code.file <- paste(scan(file=code.files,sep='\n',what='character',quiet=TRUE),collapse='\n')
+   #get .sas files
+   code.files <- paste(SAScache.directory,files[grepl('.sas',files)],sep='/')
+   code.file <- paste(scan(file=code.files,sep='\n',what='character',quiet=TRUE),collapse='\n')
 
    ##results in a dataset (assumed to be CSV)
-   #if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
-   #   output.files.short <- files[sapply(1:length(files),function(j) sum(sapply(1:length(SAS2R.names),function(i) grepl(SAS2R.names[i],files[j]))))  > 0]
-   #}else{
-   #   output.files.short <- files[grepl(SAS2R.type,files)]
-   #}
-   #output.files <- paste(SAScache.directory,output.files.short,sep='/')
-   #output.sets <- lapply(1:length(output.files), function(i) read.csv(file=output.files[i]))
-   #if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
-   #   names(output.sets) <- SAS2R.names
-   #}
-   #output2R <- new('snatchOutput',SAS2R=output.sets)
+   if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
+      output.files.short <- files[sapply(1:length(files),function(j) sum(sapply(1:length(SAS2R.names),function(i) grepl(SAS2R.names[i],files[j]))))  > 0]
+   }else{
+      output.files.short <- files[grepl(SAS2R.type,files)]
+   }
 
-   ##log files imported into R
+   output.files <- paste(SAScache.directory,output.files.short,sep='/')
+   output.sets <- lapply(1:length(output.files), function(i) read.csv(file=output.files[i]))
+
+   if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
+      names(output.sets) <- SAS2R.names
+   }
+   output2R <- new('snatchOutput',SAS2R=output.sets)
+
+   #log files imported into R
    #log.files <- paste(SAScache.directory,files[grepl('.log',files)],sep='/')
    #log.file <- paste(scan(file=log.files,sep='\n',what='character',quiet=TRUE),sep='\n')
+   log.file <- ''
 
-   #SASnatch.object <- new('SASnatch',code = code.file, results = SASnatch.results, out = output2R, log=log.file)
-   #SASnatch.object
-   #return(SASnatch.object)
+   SASnatch.object <- new('SASnatch',code = code.file, results = SASnatch.results, out = output2R, log=log.file)
+   return(SASnatch.object)
 }
