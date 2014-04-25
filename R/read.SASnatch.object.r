de6d5e4a90e2577a89ec2250674b1956cf60e345
imouzon <user@imouzon.student.iastate.edu>
2014-04-25 14:07:17 -0500

    e
    e

--- R/read.SASnatch.object.r
+++ R/read.SASnatch.object.r
@@ -15,17 +15,18 @@ read.SASnatch.object<- function (chunk.name='',SAS2R.names='',SAS2R.type='csv'){
    files <- list.files(path=SAScache.directory)
    files <- files[grepl(chunk.name,files)]
 
-   return(files)
    ##get .html files
-   #html.files <- paste(SAScache.directory,files[grepl('.html',files)],sep='/')
-   #html.results <- lapply(1:length(html.files),function(i) paste(scan(file=html.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
+   html.files <- paste(SAScache.directory,files[grepl('.html',files)],sep='/')
+   html.results <- lapply(1:length(html.files),function(i) paste(scan(file=html.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
    ##get .tex files
-   #tex.files <- paste(SAScache.directory,files[grepl('.tex',files)],sep='/')
-   #tex.results <- lapply(1:length(tex.files),function(i) paste(scan(file=tex.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
+   tex.files <- paste(SAScache.directory,files[grepl('.tex',files)],sep='/')
+   tex.results <- lapply(1:length(tex.files),function(i) paste(scan(file=tex.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
    ##make new snatchResults S4 object
    #SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results)
+
+   return(tex.results)
 
    ##get .sas files
    #code.files <- paste(SAScache.directory,files[grepl('.sas',files)],sep='/')
