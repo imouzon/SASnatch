6dca7e56bd500368f832e08cc1e394c84ef4a5a6
imouzon <user@Ians-MacBook-Air.local>
2014-04-26 22:36:27 -0500

    fix S4 objects
    fixee

--- R/read.SASnatch.object.r
+++ R/read.SASnatch.object.r
@@ -29,10 +29,10 @@ read.SASnatch.object<- function (chunk.name='',SAS2R.names='',SAS2R.type='csv'){
    output.files <- paste(SAScache.directory,output.files.short,sep='/')
    output.sets <- lapply(1:length(output.files), function(i) read.csv(file=output.files[i]))
 
-   #if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
-   #   names(output.sets) <- SAS2R.names
-   #}
-   #output2R <- new('snatchOutput',SAS2R=output.sets)
+   if(length(SAS2R.names) > 1 | SAS2R.names[1] != ''){
+      names(output.sets) <- SAS2R.names
+   }
+   output2R <- new('snatchOutput',SAS2R=output.sets)
    output2R <- output.sets
 
    #log files imported into R
@@ -41,6 +41,9 @@ read.SASnatch.object<- function (chunk.name='',SAS2R.names='',SAS2R.type='csv'){
    log.file <- ''
 
    #SASnatch.object <- new('SASnatch',code = code.file, results = SASnatch.results, out = output2R, log=log.file)
-   SASnatch.object <- list('code'=code.file, 'results' = SASnatch.results, 'out' = output2R, 'log' = log.file)
+   SASnatch.object <- list('code' = code.file, 
+                           'results' = SASnatch.results, 
+                           'out' = output2R, 
+                           'log' = log.file)
    return(SASnatch.object)
 }
