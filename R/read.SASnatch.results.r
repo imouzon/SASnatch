5c9dd06d1ded3090da2be3f5d081a7b245f2111d
imouzon <user@Ians-MacBook-Air.local>
2014-04-27 02:55:57 -0500

    true results
    d

--- R/read.SASnatch.results.r
+++ R/read.SASnatch.results.r
@@ -18,15 +18,21 @@ read.SASnatch.results<- function (chunk.name='',SASresults.path = '',SAS2R.names
    files <- list.files(path=SAScache.directory)
    files <- unique(files[grepl(chunk.name,files)])
 
-   #get .html files
+   #"true results are results that output a table
    html.files <- paste(SAScache.directory,files[grepl('.html',files)],sep='/')
+   r.results <- lapply(1:length(html.files),function(i) readHTMLTable(html.results[[i]]))
+   true.results <- sapply(1:length(html.files), function(i) length(r.results))
+
+   #get only the .html files that have tables in them (the "true" results)
+   html.files <- html.files[true.results]
    html.results <- lapply(1:length(html.files),function(i) paste(scan(file=html.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
-   #get .tex files
+   #get .tex files using the corresponding "true" html files
    tex.files <- paste(SAScache.directory,files[grepl('.tex',files)],sep='/')
+   tex.files <- tex.files[true.results]
    tex.results <- lapply(1:length(tex.files),function(i) paste(scan(file=tex.files[i],sep='\n',what='character',quiet=TRUE),collapse='\n'))
 
-   #get simple text results as R tables
+   #get simple text results as R tables using "true" results
    r.results <- lapply(1:length(html.files),function(i) readHTMLTable(html.results[[i]]))
 
    #make new snatchResults S4 object
