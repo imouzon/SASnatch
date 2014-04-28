87bf1fbcc20964a2299cbe10b864e3e89570dcff
imouzon <user@Ians-MacBook-Air.local>
2014-04-28 13:11:18 -0500

    dogs

---
+++ R/printSASnatch.r
@@ -0,0 +1,30 @@
+#' Print SASnatch results
+#'
+#' @param working.directory character value, optional argument
+#' @param chunk.name character value, optional argument
+#' @export
+printSASnatch <- function(SASnatch.S4,type='R'){
+   if(type == 'R'){
+   for(i in 1:length(SASnatch@results@R)){
+      #Footer templates
+      for(j in 1:length(SASnatch@results@R[[i]])){
+         print(SASnatch@results@R[[i]][[j]])
+      }
+   }
+
+   if(type == 'TeX'){
+   for(i in 1:length(SASnatch@results@TeX)){
+      #Footer templates
+      for(j in 1:length(SASnatch@results@TeX[[i]])){
+         print(SASnatch@results@TeX[[i]][[j]])
+      }
+   }
+
+   if(type == 'HTML'){
+   for(i in 1:length(SASnatch@results@HTML)){
+      #Footer templates
+      for(j in 1:length(SASnatch@results@HTML[[i]])){
+         print(SASnatch@results@HTML[[i]][[j]])
+      }
+   }
+}
