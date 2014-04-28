90b757e2dc1ffd75c7fd91ea27026fc78b2323e3
imouzon <user@Ians-MacBook-Air.local>
2014-04-28 14:19:50 -0500

    f

--- R/read.SASnatch.results.r
+++ R/read.SASnatch.results.r
@@ -38,7 +38,7 @@ read.SASnatch.results<- function (chunk.name='',SASresults.path = '',SAS2R.names
       r.results <- lapply(1:length(html.files),function(i) readHTMLTable(html.files[i]))
 
       #many of these results will not be tables
-      true.results <- sapply(1:length(html.files), function(i) length(r.results))
+      true.results <- sapply(1:length(html.files), function(i) length(r.results[[i]])>0)
 
       #get only the .html files that have tables in them (the "true" results)
       html.files <- html.files[true.results]
@@ -52,6 +52,7 @@ read.SASnatch.results<- function (chunk.name='',SASresults.path = '',SAS2R.names
    }
 
    #make new snatchResults S4 object
-   SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results, R = r.results,files = paste(html.files,sep='\n'))
+   #SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results, R = r.results,files = paste(html.files,sep='\n'))
+   SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results, R = r.results,files = true.results)
    return(SASnatch.results)
 }
