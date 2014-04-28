a6d4a5b87af0cc3b1493a10193110c81acd8a751
imouzon <user@Ians-MacBook-Air.local>
2014-04-28 14:07:23 -0500

    f

--- R/read.SASnatch.results.r
+++ R/read.SASnatch.results.r
@@ -53,5 +53,6 @@ read.SASnatch.results<- function (chunk.name='',SASresults.path = '',SAS2R.names
 
    #make new snatchResults S4 object
    SASnatch.results <- new('snatchResults', HTML = html.results, TeX = tex.results, R = r.results)
-   return(SASnatch.results)
+   #return(SASnatch.results)
+   return(html.files)
 }
