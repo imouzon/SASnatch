331e04c7f0083c58440976483d846431e6b421d2
imouzon <user@Ians-MacBook-Air.local>
2014-04-29 09:53:47 -0500

    f

--- R/addSASfooter.r
+++ R/addSASfooter.r
@@ -8,6 +8,7 @@
 addSASfooter <- function(SAScache.directory='',chunk.name='unlabeled-SASnatch-chunk'){
    #Footer templates
    SAScode.ODS.CLOSE.TEMPLATE = 'ods _all_ close; ods trace off; run;'
-   SAScode.LOG.CLOSE.TEMPLATE = 'proc printto; run;'
+   #SAScode.LOG.CLOSE.TEMPLATE = 'proc printto; run;'
+   SAScode.LOG.CLOSE.TEMPLATE = ''
    return(paste(SAScode.ODS.CLOSE.TEMPLATE,SAScode.LOG.CLOSE.TEMPLATE,sep='\n\n'))
 }
