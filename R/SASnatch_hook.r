c31686bf1b4b38f090070536979e5d8924ce2738
imouzon <user@Ians-MacBook-Air.local>
2014-04-30 10:22:39 -0500

    f

--- R/SASnatch_hook.r
+++ R/SASnatch_hook.r
@@ -53,7 +53,7 @@ SASnatch_hook = function(before, options, envir){
       system(SASnatch.SASRUN)
 
       #read the SASnatch output
-      SASnatch.S4 <<- read.SASnatch.object(chunk.name=chunk.name, SASresults.path=SAScache.directory, SAS2R.names=SAS2R,SAS2R.type='.csv')
+      #SASnatch.S4 <<- read.SASnatch.object(chunk.name=chunk.name, SASresults.path=SAScache.directory, SAS2R.names=SAS2R,SAS2R.type='.csv')
       #Change the name of the S4 object
       #eval(parse(text=paste(chunk.name,'.snatch <<- SASnatch.S4',sep='')))
    }else{
