5f32ba424c0cbed367344a551b2d3e5ccb928d4c
imouzon <user@imouzon.student.iastate.edu>
2014-04-30 11:35:52 -0500

    fixed code

--- R/SASnatch_hook.r
+++ R/SASnatch_hook.r
@@ -20,7 +20,7 @@ SASnatch_hook = function(before, options, envir){
 
       #get the code from the chunk
       if(!is.null(knitr:::opts_current$get('code'))){
-         code = knitr:::opts_current$get('code')
+         code = paste(knitr:::opts_current$get('code'),sep = '\n')
       }
 
       #determine which datasets go to SAS and which come from SAS 
